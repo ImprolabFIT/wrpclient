39295859094a8b398cb40bc63a5eb1c1313358e4
Petr Kasalicky <kasalpe1@fit.cvut.cz>
2020-01-31 21:12:16 +0100

    Fixed README to be compatible with PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-with open('docs/source/index.rst', 'r') as f:
+with open('README.rst', 'r') as f:
     long_description = ''.join(f.readlines())
 
 setup(
