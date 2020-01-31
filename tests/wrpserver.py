8b444795a26b0b442ad8dacf0eb678207d53c3be
Petr Kasalicky <kasalpe1@fit.cvut.cz>
2020-01-31 20:51:36 +0100

    Added more features to test server (allow to set cameras and frames)

--- tests/wrpserver.py
+++ tests/wrpserver.py
@@ -16,7 +16,7 @@ def randomString(stringLength=10):
 
 class WRPServer:
 
-	DEFAULT_PORT = 8765
+	DEFAULT_PORT = 8766
 	SERVER_IP_ADDRESS = "127.0.0.1"
 	class FakeCamera:
 		def __init__(self):
@@ -41,42 +41,45 @@ class WRPServer:
 		self.camera_list = WRPServer.generate_random_valid_camera_list(5)
 		self.state = WRPServer.State.IDLE; 
 
+	async def __handle_server_exception__(self):
+		async with self.server as s:
+			try:
+				await s.serve_forever()
+			except asyncio.CancelledError:
+				pass
+			except BaseException as e:
+				print("Catched exception from the server:", e)
+
 	async def run(self):
-		print("Starting the server")
 		self.server = await asyncio.start_server(self.routine, self.ip_address, self.port)
-		print("Server has been started")
-		async with self.server:
-			await self.server.serve_forever()
+		self.server_task = asyncio.create_task(self.__handle_server_exception__())
 
 	async def routine(self, reader, writer):
-		self.__reader = reader
-		self.__writer = writer
-		print("Routine has started")
 		self.state = WRPServer.State.CONNECTED
 		while(True):
-			print("Waiting for the message...")
-			message = await self.receive_message()
+			print("Server: Waiting for the message...")
+			message = await WRPServer.receive_message(reader)
 			message_to_send = self.handle_message(message)
 			if(message_to_send is not None):
-				await self.send_message(message_to_send)
+				await WRPServer.send_message(writer, message_to_send)
 			else:
 				print("No response to be sent was generated")
 
-		writer.close()
-
 	async def stop(self):
-		await self.server.shutdown()
-		self.server.server_close()
-		del self.__reader
-		del self.__writer
+		self.server.close()
+		await self.server.wait_closed()
 		del self.server 
+		self.server_task.cancel()
+		await self.server_task
+		del self.server_task
+
 
 	def handle_message(self, message):
 		to_send = None
-		print(f"Handling message {message} in state {self.state}")
+		print(f"Server: Handling message {message} in state {self.state}")
 		if(message.msg_type == Message.Type.GET_CAMERA_LIST):
-			if(self.state == WRPServer.State.CONNECTED):				
-				to_send = Message.create_message(Message.Type.CAMERA_LIST, xml_camera_list=self.xml_camera_list)
+			if(self.state == WRPServer.State.CONNECTED):
+				to_send = Message.create_message(Message.Type.CAMERA_LIST, xml_camera_list=WRPServer.camera_list_to_xml(self.camera_list))
 			else:
 				to_send = Message.create_message(Message.Type.ERROR, error_code=Message.ErrorCode.UNEXPECTED_MESSAGE)
 
@@ -84,20 +87,7 @@ class WRPServer:
 			if(self.state == WRPServer.State.CONNECTED):
 				serial_number = getattr(message, Message.CAMERA_SERIAL_NUMBER_ATTR_NAME)
 				try:
-					camera = [c for c in self.camera_list if c.serial_number == serial_number][0]
-					self.state = WRPServer.State.CAMERA_SELECTED
-					self.camera_selected = camera
-					to_send = Message.create_message(Message.Type.OK)
-				except IndexError:
-					to_send = Message.create_message(Message.Type.ERROR, error_code=Message.ErrorCode.CAMERA_NOT_FOUND)
-			else:
-				to_send = Message.create_message(Message.Type.ERROR, error_code=Message.ErrorCode.UNEXPECTED_MESSAGE)
-
-		elif(message.msg_type == Message.Type.OPEN_CAMERA):			
-			if(self.state == WRPServer.State.CONNECTED):
-				serial_number = getattr(message, Message.CAMERA_SERIAL_NUMBER_ATTR_NAME)
-				try:
-					camera = [c for c in self.camera_list if c.serial_number == serial_number][0]
+					camera = [c for c in self.camera_list if c.SerialNumber == serial_number][0]
 					self.state = WRPServer.State.CAMERA_SELECTED
 					self.camera_selected = camera
 					to_send = Message.create_message(Message.Type.OK)
@@ -108,47 +98,39 @@ class WRPServer:
 
 		elif(message.msg_type == Message.Type.CLOSE_CAMERA):			
 			if(self.state == WRPServer.State.CAMERA_SELECTED):
-				serial_number = getattr(message, Message.CAMERA_SERIAL_NUMBER_ATTR_NAME)
-				try:
-					camera = [c for c in self.camera_list if c.serial_number == serial_number][0]
-					self.state = WRPServer.State.CONNECTED
-					del self.camera_selected 
-					to_send = Message.create_message(Message.Type.OK)
-				except IndexError:
-					to_send = Message.create_message(Message.Type.ERROR, error_code=Message.ErrorCode.CAMERA_NOT_FOUND)
+				self.state = WRPServer.State.CONNECTED
+				del self.camera_selected
+				to_send = Message.create_message(Message.Type.OK)
 			else:
 				to_send = Message.create_message(Message.Type.ERROR, error_code=Message.ErrorCode.UNEXPECTED_MESSAGE)
 
 		elif(message.msg_type == Message.Type.GET_FRAME):			
 			if(self.state == WRPServer.State.CAMERA_SELECTED):
-				serial_number = getattr(message, Message.CAMERA_SERIAL_NUMBER_ATTR_NAME)
-				try:
-					camera = [c for c in self.camera_list if c.serial_number == serial_number][0]
-					to_send = Message.create_message(Message.Type.FRAME, )
-				except IndexError:
-					to_send = Message.create_message(Message.Type.ERROR, error_code=Message.ErrorCode.CAMERA_NOT_FOUND)
+				print("Server: frame_dict:", self.frame_dict)
+				to_send = Message.create_message(Message.Type.FRAME, **self.frame_dict)
 			else:
 				to_send = Message.create_message(Message.Type.ERROR, error_code=Message.ErrorCode.UNEXPECTED_MESSAGE)
-		
 		else:
 			print(f"Unhandled message type {message.msg_type}")
 		return to_send
 
-	async def send_message(self, message):
-		print("Message to send:", message)
-		self.__writer.write(message.encode())
-		await self.__writer.drain()
+	@staticmethod
+	async def send_message(writer, message):
+		print("Server: Message to send:", message)
+		writer.write(message.encode())
+		await writer.drain()
 
-	async def receive_message(self):
-		message_type_value, payload_length = struct.unpack(">BI", await self.__reader.readexactly(Message.MESSAGE_TYPE_LENGTH + Message.PAYLOAD_SIZE_LENGTH))
+	@staticmethod
+	async def receive_message(reader):
+		message_type_value, payload_length = struct.unpack(">BI", await reader.readexactly(Message.MESSAGE_TYPE_LENGTH + Message.PAYLOAD_SIZE_LENGTH))
+		print(f"Server: Received message type value: {message_type_value}, payload_length: {payload_length}")
 		if(payload_length > 0):
-			payload = await self.__reader.readexactly(payload_length)
+			payload = await reader.readexactly(payload_length)
 		else:
 			payload = bytes()
 		response = Message.create_message_from_buffer(message_type_value=message_type_value, payload=payload, payload_length=payload_length)
-		print("Received message:", response)
+		print("Server: Received message:", response)
 		return response
-	
 
 	@staticmethod
 	def generate_random_valid_camera_list(number_of_cameras):
@@ -163,8 +145,8 @@ class WRPServer:
 
 	@staticmethod
 	def generate_random_frame(camera):
-		width = getattr(camera, "Width")
-		height = getattr(camera, "Height")
+		width = getattr(camera, "width")
+		height = getattr(camera, "height")
 		frame_np =  (np.random.random((100, 100)) * 100).astype(np.float32)
 		return { 
 			Message.FRAME_NUMBER_ATTR_NAME: np.random.randint(0, 10), 
